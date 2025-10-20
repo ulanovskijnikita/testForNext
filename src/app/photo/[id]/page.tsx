fd66df419ddb31b7c6d044ca92156d5dd4a35df1
ulanovskijnikita <ulanovskijnikita@gmail.com>
2025-10-20 19:43:51 +0500

    chore: add console log with tester2

--- src/app/photo/[id]/page.tsx
+++ src/app/photo/[id]/page.tsx
@@ -2,6 +2,7 @@ import Pathname from "./Pathname"
 
 const Photo = async ({ params }: PageProps<'/photo/[id]'>) => {
 
+    console.log('tester2')
     console.log('tester')
     console.log('load dynamic')
     await new Promise((res) => setTimeout(() => {res(null)}, 2000))
